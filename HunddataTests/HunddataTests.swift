41c1dc917378cb3b24bebc240ab7369a1db89219
Han Lin Yap <codler@gmail.com>
2015-01-09 19:59:24 +0100

    Initial Commit

---
+++ HunddataTests/HunddataTests.swift
@@ -0,0 +1,36 @@
+//
+//  HunddataTests.swift
+//  HunddataTests
+//
+//  Created by Han Lin Yap on 2015-01-09.
+//  Copyright (c) 2015 Han Lin Yap. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class HunddataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
